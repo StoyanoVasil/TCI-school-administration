64e357f24f86f217e3d8b05ddc3944588b6c7e61
i333656 <vasil.stoyanov@feg.bg>
2018-11-24 16:21:26 +0100

    Add course end/start date test

---
+++ src/main/java/CourseDateException.java
@@ -0,0 +1,24 @@
+/**
+ * Custom exception class.
+ * Used when the end date of a Course is before the start date
+ */
+public class CourseDateException extends Exception {
+
+    public CourseDateException() {}
+
+    public CourseDateException(String message) {
+        super(message);
+    }
+
+    public CourseDateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CourseDateException(Throwable cause) {
+        super(cause);
+    }
+
+    public CourseDateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -1,2 +1,20 @@
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class CourseTest {
+
+    @Test(expected=CourseDateException.class)
+    public void testEndDateBeforeStartDate() throws CourseDateException {
+
+        // arrange
+        String courseName = "TCI";
+        Date startDate = new GregorianCalendar(2019, Calendar.FEBRUARY, 15).getTime();
+        Date endDate = new GregorianCalendar(2018, Calendar.NOVEMBER, 12).getTime();
+
+        // act
+        Course course = new Course(courseName, startDate, endDate);
+    }
 }
