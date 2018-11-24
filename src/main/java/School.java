a42825b4c528e31e490fc7d1c2da4d812672981c
i333656 <vasil.stoyanov@feg.bg>
2018-11-24 16:24:23 +0100

    Add school fields implementation

--- src/main/java/School.java
+++ src/main/java/School.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class School {
+
+    private String name;
+    private Date openingDate;
+    private ArrayList<Course> courses;
+
+    public School(String name, Date openingDate) {
+        this.name = name;
+        this.openingDate = openingDate;
+        this.courses = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getOpeningDate() {
+        return openingDate;
+    }
+
+    public int getNumberOfCourses() {
+        return this.courses.size();
+    }
 }
