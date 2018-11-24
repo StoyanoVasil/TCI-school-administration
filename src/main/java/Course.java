1bdada14412ca945e5aa985735f2ee026b4d0e2d
i333656 <vasil.stoyanov@feg.bg>
2018-11-24 16:22:33 +0100

    Add course end/start date implementation

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,2 +1,30 @@
+import java.util.Date;
+
 public class Course {
+
+    private String name;
+    private Date startDate;
+    private Date endDate;
+
+    public Course(String name, Date startDate, Date endDate) throws CourseDateException {
+        if(startDate.before(endDate)) {
+            this.name = name;
+            this.startDate = startDate;
+            this.endDate = endDate;
+        } else {
+            throw new CourseDateException("Start date should be before end date!");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
 }
