0601ab880f5c9f01225fdd1a0f5deb96221f962e
i333656 <vasil.stoyanov@feg.bg>
2018-11-24 16:23:32 +0100

    Add school fields test

--- src/test/java/SchoolTest.java
+++ src/test/java/SchoolTest.java
@@ -1,2 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.assertEquals;
+
 public class SchoolTest {
+
+    private String schoolName;
+    private Date openingDate;
+    private School school;
+
+    @Before
+    public void before() {
+
+        // arrange
+        schoolName = "Fontys";
+        openingDate = new GregorianCalendar(2018, Calendar.AUGUST, 28).getTime();
+        school = new School(schoolName, openingDate);
+    }
+
+    @Test
+    public void testSchoolConstructorInitiatesAllField() {
+
+        // assert
+        assertEquals("School name is not set", this.schoolName, school.getName());
+        assertEquals("School opening date is not set", this.openingDate, school.getOpeningDate());
+        assertEquals("School opening date is not set", 0, school.getNumberOfCourses());
+    }
 }
